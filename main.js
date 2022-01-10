523a2aac586d049a7fb9bb9ec105dd9bcaeaad9e
yuraup <o721@naver.com>
2022-01-10 17:37:16 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let min = 0;
+let sec = 0;
+let ms = 0; // 시간의 초기값을 설정합니다.
+
+// 각 변수 별로 html에서 element를 가져오는 코드를 작성하세요 (*변수명을 수정하지 말아주세요)
+const millisec = 
+const second = 
+const minutes =
+
+const startBtn =
+const stopBtn = 
+const resetBtn = 
+
+//시작 버튼 클릭 시 시간이 10ms씩 증가합니다.
+ 
+//일시정지 버튼 클릭 시 시간 증가가 정지됩니다.
+
+//리셋 버튼 클릭 시 시간이 상태로 초기화 됩니다.
